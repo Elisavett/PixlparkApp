3ec07421b1a71ac5acb53d414d617e06b48f5ab7
Elisavett <lizayakobi@mail.ru>
2021-08-19 08:27:32 +0700

    Добавление структуры MVC

---
+++ PixlparkApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace PixlparkApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+            return View();
+        }
+    }
+}
